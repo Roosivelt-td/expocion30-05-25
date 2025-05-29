f68658c1d8c8a67a27fb00323c3dadecfa6d7ff3
Roosivelt tupia <roosivelt.td@gmail.com>
2025-05-29 11:48:11 -0500

    expocion

---
+++ src/main/java/com/expocicion/elp/ExpociconApplication.java
@@ -0,0 +1,13 @@
+package com.expocicion.elp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpociconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpociconApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expocicion/elp/ExpociconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expocicion.elp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpociconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
